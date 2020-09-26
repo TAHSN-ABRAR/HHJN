e6168df11d883dc12af4f81e6dbfeb56b1f766b8
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2020-09-27 00:10:12 +0600

    Update REGISTRATION.c

--- IMON/REGISTRATION.c
+++ IMON/REGISTRATION.c
@@ -1,11 +1,16 @@
+///Registration
+
 #include<stdio.h>
 #include<string.h>
 #include<conio.h>
 
 struct regi
 {
-    char  phone[12],name[20],pass[50];
+    char name[20],pass[50];
 };
+
+
+//void registration(){};
 
 void registration()
 {
@@ -15,29 +20,27 @@ void registration()
     FILE *regiDB;
     regiDB=fopen(filepath,"r");
     int i=1;
-    while(fscanf(regiDB,"%s",&Old[i].phone)==1)
+    while(fscanf(regiDB,"%s",&Old[i].name)==1)
     {
-        fscanf(regiDB,"%s%s",&Old[i].name,&Old[i].pass);
+        fscanf(regiDB,"%s",&Old[i].pass);
         i++;
     }
     int noclient=i;
     fclose(regiDB);
     regiDB=fopen(filepath,"a");
-    printf("Enter your phone Number : ");
-start1:
-    scanf("%s",New.phone);
-    for(i=1; i<noclient; i++)
-    {
-        if(strcmp(Old[i].phone,New.phone)==0)
-        {
-            printf("\aThe Number have already an account.\nEnter a New Number again : ");
-            goto start1 ;
-        }
-    }
-    printf("Enter your Name : ");
+    printf("Enter your User Name : ");
+    start1:
     scanf("%s",New.name);
+    for(i=1;i<noclient;i++)
+    {
+        if(strcmp(Old[i].name,New.name)==0)
+            {
+                printf("\aThe User Name have already an account.\nEnter a New User Name again : ");
+                goto start1 ;
+            }
+    }
     printf("Enter a Password : ");
-start2:
+    start2:
     i=0;
     char ch,confirmPasss[50];
     while(1)
@@ -68,8 +71,8 @@ start2:
 
 
     printf("\nConfirm Your Password : ");
-start3 :
-    i=0;
+    start3 :
+i=0;
     while(1)
     {
         ch=getch();
@@ -98,14 +101,13 @@ start3 :
     if(strcmp(New.pass,confirmPasss)!=0)
     {
         printf("\nPassword Not match.\n");
-start4:
+        start4:
         printf("Do you forget your password :\n1.Yes\n2.No\n");
         printf("Enter Your choice (1 or 2) : ");
         int c;
         scanf("%d",&c);
-        if(c==1)
-        {
-            printf("Enter a New Password : ");
+        if(c==1){
+                printf("Enter a New Password : ");
             goto start2 ;
         }
         else if(c==2)
@@ -118,14 +120,13 @@ start4:
 
     }
     else
-    {
-        fprintf(regiDB,"%s\n%s\n%s\n",New.phone,New.name,New.pass);
-    }
-    printf("\n***************************Registration Completed***************************\n");
-    fclose(regiDB);
+        {
+            fprintf(regiDB,"%s\n%s\n",New.name,New.pass);
+        }
+        printf("\n***************************Registration Completed***************************\n");
+      fclose(regiDB);
 }
 int main()
 {
     registration();
 }
-
