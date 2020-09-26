aa09d5ee6442c2b90c85d0f19b1c4869774dbb1a
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2020-09-27 00:16:16 +0600

    Update ADD FRIEND.c

--- FAHAD/ADD FRIEND.c
+++ FAHAD/ADD FRIEND.c
@@ -2,7 +2,7 @@
 #include<string.h>
 struct friendinfo
 {
-    char name[100],pass[100];
+    char name[100];
 };
 void printmassage(char *str)
 {
@@ -10,29 +10,50 @@ void printmassage(char *str)
     printf("%s", str);
     printf("***\n\n");
 }
-
 int menu()
 {
     int choice;
-    printf("1. ADD FRINDS.\n");
+    printf("1. MESSAGE\n");
+    printf("2. SHOW FRIENDS\n");
+    printf("3. ADD FRIEND\n");
+    printf("4. LOGOUT\n>>>");
     scanf("%d",&choice);
-    printf("\n\n");
     return choice;
+}
+void Login()
+{
+
+}
+void Registration()
+{
+
+}
+void messages(int n)
+{
+    printf("Returning value %d\n\n",n);
+}
+void showfriend(int n)
+{
+
+}
+void logout(int n)
+{
+
 }
 void addfrnd()
 {
     int i=0,td,choice,flag;
-    char nm[100],pass[100];
+    char nm[100];
     FILE * db;
     struct friendinfo flist[100];
-    db=fopen("friendfile.txt","a");
+    db=fopen("friend_name.txt","a");
     if(!db)
     {
         printf("File openning error\n Please try again\n\n");
         return;
     }
     fclose(db);
-    db=fopen("friendfile.txt","r");
+    db=fopen("friend_name.txt","r");
     if(!db)
     {
         printf("File openning error\n Please try again\n\n");
@@ -40,26 +61,23 @@ void addfrnd()
     }
     while(fscanf(db,"%s",flist[i].name)==1)
     {
-        fscanf(db,"%s",flist[i].pass);
         i++;
     }
     td=i;
 
     while(1)
     {
-        printf("Enter Your Friend Name : ");
+        printf("Enter Your Friend's User Name : ");
         scanf("%s",nm);
-        printf("Enter password : ");
-        scanf("%s",pass);
         for(i=0,flag=0; i<td; i++)
         {
 
             if(strcmp(nm,flist[i].name)==0)
             {
-                printmassage("This Friend is Already added");
+                printmassage("This name is currently in use***\n***Please Try Again");
                 printf("#Do You Want To Add Again??\n");
                 printf("  1.YES\n");
-                printf("  2.NO\n");
+                printf("  2.NO\n>>>");
                 scanf("%d",&choice);
                 if(choice==1)
                     flag=1;
@@ -69,13 +87,13 @@ void addfrnd()
 
             }
         }
-        if(flag==2)break;
+        if(flag==2)
+            break;
         if(flag==0)
         {
 
-            db=fopen("friendfile.txt","a");
+            db=fopen("friend_name.txt","a");
             fprintf(db,"%s\n",nm);
-            fprintf(db,"%s\n",pass);
             printmassage("successfully added");
             fclose(db);
             break;
@@ -84,20 +102,47 @@ void addfrnd()
 
     fclose(db);
 }
+int firstmenu()
+{
+    int choice;
+    printf("1.LOGIN\n");
+    printf("2.REGISTRATION\n>>>");
+    scanf("%d",&choice);
 
+    switch(choice)
+    {
+    case 1:
+        Login();
+        break;
+    case 2:
+        Registration();
+        break;
+    }
+    return ;
+}
 int main()
 {
     int choice;
+    choice=firstmenu();
     do
     {
         choice = menu();
         switch(choice)
         {
         case 1:
+            messages(choice);
+            break;
+        case 2:
+            showfriend(choice);
+            break;
+        case 3:
             addfrnd();
             break;
-        }
+        case 4:
+            logout(choice);
+            break;
 
+        }
     }
-    while(choice!=2);
+    while(choice!=5);
 }
